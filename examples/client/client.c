cb418c122661ae914df04070d642823935d972c2
John Safranek <john@wolfssl.com>
2017-07-24 13:45:46 -0700

    Example Refactoring 1. Added a test.h header similar to wolfSSL's. 2. Added headers for example echoserver and client. 3. Refactored the client and echoserver to be similar to wolfSSL's where    the code may be included without the main function in another    executable, for example, the test. 4. Added options to the client for setting the host address and port    number.

--- examples/client/client.c
+++ examples/client/client.c
@@ -19,283 +19,64 @@
  */
 
 
-#include <ctype.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#ifndef SO_NOSIGPIPE
-    #include <signal.h>
-#endif
-
 #include <wolfssh/ssh.h>
+#include <wolfssh/test.h>
+#include "examples/client/client.h"
 
 
 const char testString[] = "Hello, wolfSSH!";
-#define SERVER_PORT_NUMBER      22222
-
-
-#ifdef __GNUC__
-    #define WS_NORETURN __attribute__((noreturn))
-#else
-    #define WS_NORETURN
-#endif
-
-static INLINE WS_NORETURN void err_sys(const char* msg)
-{
-    printf("client error: %s\n", msg);
-
-#ifndef __GNUC__
-    /* scan-build (which pretends to be gnuc) can get confused and think the
-     * msg pointer can be null even when hardcoded and then it won't exit,
-     * making null pointer checks above the err_sys() call useless.
-     * We could just always exit() but some compilers will complain about no
-     * possible return, with gcc we know the attribute to handle that with
-     * WS_NORETURN. */
-    if (msg)
-#endif
-    {
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-typedef int SOCKET_T;
-#ifdef TEST_IPV6
-    typedef struct sockaddr_in6 SOCKADDR_IN_T;
-    #define AF_INET_V           AF_INET6
-    static const char*          wolfsshIP = "::1";
-#else
-    typedef struct sockaddr_in  SOCKADDR_IN_T;
-    #define AF_INET_V           AF_INET
-    static const char*          wolfsshIP = "127.0.0.1";
-#endif
-
-#if defined(__MACH__) || defined(USE_WINDOWS_API)
-    #ifndef _SOCKLEN_T
-        typedef int socklen_t;
-    #endif
-#endif
-/* HPUX doesn't use socklent_t for third parameter to accept, unless
-   _XOPEN_SOURCE_EXTENDED is defined */
-#if !defined(__hpux__) && !defined(CYASSL_MDK_ARM) && !defined(CYASSL_IAR_ARM)
-    typedef socklen_t SOCKLEN_T;
-#else
-    #if defined _XOPEN_SOURCE_EXTENDED
-        typedef socklen_t SOCKLEN_T;
-    #else
-        typedef int       SOCKLEN_T;
-    #endif
-#endif
-
 
 
 static void ShowUsage(void)
 {
     printf("client %s\n", LIBWOLFSSH_VERSION_STRING);
     printf(" -?          display this help and exit\n");
+    printf(" -h <host>   host to connect to, default %s\n", wolfSshIp);
+    printf(" -p <num>    port to connect on, default %d\n", wolfSshPort);
 }
 
 
-static INLINE void build_addr(SOCKADDR_IN_T* addr, const char* peer,
-                              uint16_t port)
-{
-    int useLookup = 0;
-    (void)useLookup;
-
-    memset(addr, 0, sizeof(SOCKADDR_IN_T));
-
-#ifndef TEST_IPV6
-    /* peer could be in human readable form */
-    if ( (peer != INADDR_ANY) && isalpha((int)peer[0])) {
-        #ifdef CYASSL_MDK_ARM
-            int err;
-            struct hostent* entry = gethostbyname(peer, &err);
-        #else
-            struct hostent* entry = gethostbyname(peer);
-        #endif
-
-        if (entry) {
-            memcpy(&addr->sin_addr.s_addr, entry->h_addr_list[0],
-                   entry->h_length);
-            useLookup = 1;
-        }
-        else
-            err_sys("no entry for host");
-    }
-#endif
-
-#ifndef TEST_IPV6
-    #if defined(CYASSL_MDK_ARM)
-        addr->sin_family = PF_INET;
-    #else
-        addr->sin_family = AF_INET_V;
-    #endif
-    addr->sin_port = htons(port);
-    if (peer == INADDR_ANY)
-        addr->sin_addr.s_addr = INADDR_ANY;
-    else {
-        if (!useLookup)
-            addr->sin_addr.s_addr = inet_addr(peer);
-    }
-#else
-    addr->sin6_family = AF_INET_V;
-    addr->sin6_port = htons(port);
-    if (peer == INADDR_ANY)
-        addr->sin6_addr = in6addr_any;
-    else {
-        #ifdef HAVE_GETADDRINFO
-            struct addrinfo  hints;
-            struct addrinfo* answer = NULL;
-            int    ret;
-            char   strPort[80];
-
-            memset(&hints, 0, sizeof(hints));
-
-            hints.ai_family   = AF_INET_V;
-            hints.ai_socktype = udp ? SOCK_DGRAM : SOCK_STREAM;
-            hints.ai_protocol = udp ? IPPROTO_UDP : IPPROTO_TCP;
-
-            SNPRINTF(strPort, sizeof(strPort), "%d", port);
-            strPort[79] = '\0';
-
-            ret = getaddrinfo(peer, strPort, &hints, &answer);
-            if (ret < 0 || answer == NULL)
-                err_sys("getaddrinfo failed");
-
-            memcpy(addr, answer->ai_addr, answer->ai_addrlen);
-            freeaddrinfo(answer);
-        #else
-            printf("no ipv6 getaddrinfo, loopback only tests/examples\n");
-            addr->sin6_addr = in6addr_loopback;
-        #endif
-    }
-#endif
-}
-
-
-static INLINE void tcp_socket(SOCKET_T* sockFd)
-{
-    *sockFd = socket(AF_INET_V, SOCK_STREAM, 0);
-
-#ifdef USE_WINDOWS_API
-    if (*sockFd == INVALID_SOCKET)
-        err_sys("socket failed\n");
-#else
-    if (*sockFd < 0)
-        err_sys("socket failed\n");
-#endif
-
-#ifndef USE_WINDOWS_API
-#ifdef SO_NOSIGPIPE
-    {
-        int       on = 1;
-        socklen_t len = sizeof(on);
-        int       res = setsockopt(*sockFd, SOL_SOCKET, SO_NOSIGPIPE, &on, len);
-        if (res < 0)
-            err_sys("setsockopt SO_NOSIGPIPE failed\n");
-    }
-#elif defined(CYASSL_MDK_ARM)
-    /* nothing to define */
-#else  /* no S_NOSIGPIPE */
-    signal(SIGPIPE, SIG_IGN);
-#endif /* S_NOSIGPIPE */
-
-#if defined(TCP_NODELAY)
-    {
-        int       on = 1;
-        socklen_t len = sizeof(on);
-        int       res = setsockopt(*sockFd, IPPROTO_TCP, TCP_NODELAY, &on, len);
-        if (res < 0)
-            err_sys("setsockopt TCP_NODELAY failed\n");
-    }
-#endif
-#endif  /* USE_WINDOWS_API */
-}
-
-
-#define MY_EX_USAGE 2
-
-extern int   myoptind;
-extern char* myoptarg;
-
-static INLINE int mygetopt(int argc, char** argv, const char* optstring)
-{
-    static char* next = NULL;
-
-    char  c;
-    char* cp;
-
-    if (myoptind == 0)
-        next = NULL;   /* we're starting new/over */
-
-    if (next == NULL || *next == '\0') {
-        if (myoptind == 0)
-            myoptind++;
-
-        if (myoptind >= argc || argv[myoptind][0] != '-' ||
-                                argv[myoptind][1] == '\0') {
-            myoptarg = NULL;
-            if (myoptind < argc)
-                myoptarg = argv[myoptind];
-
-            return -1;
-        }
-
-        if (strcmp(argv[myoptind], "--") == 0) {
-            myoptind++;
-            myoptarg = NULL;
-
-            if (myoptind < argc)
-                myoptarg = argv[myoptind];
-
-            return -1;
-        }
-
-        next = argv[myoptind];
-        next++;                  /* skip - */
-        myoptind++;
-    }
-
-    c  = *next++;
-    /* The C++ strchr can return a different value */
-    cp = (char*)strchr(optstring, c);
-
-    if (cp == NULL || c == ':')
-        return '?';
-
-    cp++;
-
-    if (*cp == ':') {
-        if (*next != '\0') {
-            myoptarg = next;
-            next     = NULL;
-        }
-        else if (myoptind < argc) {
-            myoptarg = argv[myoptind];
-            myoptind++;
-        }
-        else
-            return '?';
-    }
-
-    return c;
-}
-
-
-static int runClient(void)
+THREAD_RETURN WOLFSSH_THREAD client_test(void* args)
 {
     WOLFSSH_CTX* ctx = NULL;
     WOLFSSH* ssh = NULL;
-    SOCKET_T sockFd = 0;
+    SOCKET_T sockFd = WOLFSSH_SOCKET_INVALID;
     SOCKADDR_IN_T clientAddr;
-    SOCKLEN_T clientAddrSz = sizeof(clientAddr);
+    socklen_t clientAddrSz = sizeof(clientAddr);
     char rxBuf[80];
     int ret;
+    char ch;
+    uint16_t port = wolfSshPort;
+    char* host = (char*)wolfSshIp;
+
+    int     argc = ((func_args*)args)->argc;
+    char**  argv = ((func_args*)args)->argv;
+    ((func_args*)args)->return_code = 0;
+
+    while ((ch = mygetopt(argc, argv, "?h:p:")) != -1) {
+        switch (ch) {
+            case 'h':
+                host = myoptarg;
+                break;
+
+            case 'p':
+                port = (uint16_t)atoi(myoptarg);
+                #if !defined(NO_MAIN_DRIVER) || defined(USE_WINDOWS_API)
+                    if (port == 0)
+                        err_sys("port number cannot be 0");
+                #endif
+                break;
+
+            case '?':
+                ShowUsage();
+                exit(EXIT_SUCCESS);
+
+            default:
+                ShowUsage();
+                exit(MY_EX_USAGE);
+        }
+    }
+    myoptind = 0;      /* reset for test cases */
 
     ctx = wolfSSH_CTX_new(WOLFSSH_ENDPOINT_CLIENT, NULL);
     if (ctx == NULL)
@@ -305,7 +86,7 @@ static int runClient(void)
     if (ssh == NULL)
         err_sys("Couldn't create wolfSSH session.");
 
-    build_addr(&clientAddr, wolfsshIP, SERVER_PORT_NUMBER);
+    build_addr(&clientAddr, host, port);
     tcp_socket(&sockFd);
     ret = connect(sockFd, (const struct sockaddr *)&clientAddr, clientAddrSz);
     if (ret != 0)
@@ -342,37 +123,35 @@ static int runClient(void)
 }
 
 
-int main(int argc, char** argv)
-{
-    int result = 0;
-    char ch;
+#ifndef NO_MAIN_DRIVER
 
-    #ifdef DEBUG_WOLFSSH
-        wolfSSH_Debugging_ON();
-    #endif
+    int main(int argc, char** argv)
+    {
+        func_args args;
 
-    wolfSSH_Init();
+        args.argc = argc;
+        args.argv = argv;
+        args.return_code = 0;
 
-    while ((ch = mygetopt(argc, argv, "?")) != -1) {
-        switch (ch) {
-            case 'h' :
-                ShowUsage();
-                exit(EXIT_SUCCESS);
+        StartTCP();
 
-            default:
-                ShowUsage();
-                exit(MY_EX_USAGE);
-        }
+        #ifdef DEBUG_WOLFSSH
+            wolfSSH_Debugging_ON();
+        #endif
+
+        wolfSSH_Init();
+
+        ChangeToWolfSshRoot();
+        #ifndef NO_WOLFSSH_CLIENT
+            client_test(&args);
+        #endif /* NO_WOLFSSH_CLIENT */
+
+        wolfSSH_Cleanup();
+
+        return args.return_code;
     }
-    myoptind = 0;      /* reset for test cases */
 
-    if (runClient() != 0)
-        result = 1;
+    int myoptind = 0;
+    char* myoptarg = NULL;
 
-    wolfSSH_Cleanup();
-
-    return result;
-}
-
-int myoptind = 0;
-char* myoptarg = NULL;
+#endif /* NO_MAIN_DRIVER */
--- examples/echoserver/echoserver.c
+++ examples/echoserver/echoserver.c
@@ -1,6 +1,6 @@
 /* echoserver.c
  *
- * Copyright (C) 2014-2016 wolfSSL Inc.
+ * Copyright (C) 2014-2017 wolfSSL Inc.
  *
  * This file is part of wolfSSH.
  *
@@ -19,75 +19,16 @@
  */
 
 
-#include <ctype.h>
-#include <string.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <pthread.h>
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/sha256.h>
 #include <wolfssl/wolfcrypt/coding.h>
 #include <wolfssh/ssh.h>
-#ifndef SO_NOSIGPIPE
-    #include <signal.h>
-#endif
+#include <wolfssh/test.h>
+#include "examples/echoserver/echoserver.h"
 
 
 static const char echoserverBanner[] = "wolfSSH Example Echo Server\n";
 
-typedef int SOCKET_T;
-#ifdef TEST_IPV6
-    typedef struct sockaddr_in6 SOCKADDR_IN_T;
-    #define AF_INET_V           AF_INET6
-    static const char*          wolfsshIP = "::1";
-#else
-    typedef struct sockaddr_in  SOCKADDR_IN_T;
-    #define AF_INET_V           AF_INET
-    static const char*          wolfsshIP = "127.0.0.1";
-#endif
-#define SERVER_PORT_NUMBER      22222
-#define SCRATCH_BUFFER_SIZE     1200
-
-#if defined(__MACH__) || defined(USE_WINDOWS_API)
-    #ifndef _SOCKLEN_T
-        typedef int socklen_t;
-    #endif
-#endif
-/* HPUX doesn't use socklent_t for third parameter to accept, unless
-   _XOPEN_SOURCE_EXTENDED is defined */
-#if !defined(__hpux__) && !defined(CYASSL_MDK_ARM) && !defined(CYASSL_IAR_ARM)
-    typedef socklen_t SOCKLEN_T;
-#else
-    #if defined _XOPEN_SOURCE_EXTENDED
-        typedef socklen_t SOCKLEN_T;
-    #else
-        typedef int       SOCKLEN_T;
-    #endif
-#endif
-
-
-#if defined(_POSIX_THREADS) && !defined(__MINGW32__)
-    typedef void*         THREAD_RETURN;
-    typedef pthread_t     THREAD_TYPE;
-    #define CYASSL_THREAD
-    #define INFINITE -1
-    #define WAIT_OBJECT_0 0L
-#elif defined(CYASSL_MDK_ARM)
-    typedef unsigned int  THREAD_RETURN;
-    typedef int           THREAD_TYPE;
-    #define CYASSL_THREAD
-#else
-    typedef unsigned int  THREAD_RETURN;
-    typedef intptr_t      THREAD_TYPE;
-    #define CYASSL_THREAD __stdcall
-#endif
 
 typedef struct {
     WOLFSSH* ssh;
@@ -102,242 +43,7 @@ typedef struct {
 #ifndef EXAMPLE_BUFFER_SZ
     #define EXAMPLE_BUFFER_SZ 4096
 #endif
-
-
-#ifdef __GNUC__
-    #define WS_NORETURN __attribute__((noreturn))
-#else
-    #define WS_NORETURN
-#endif
-
-
-#define MY_EX_USAGE 2
-
-extern int   myoptind;
-extern char* myoptarg;
-
-static INLINE int mygetopt(int argc, char** argv, const char* optstring)
-{
-    static char* next = NULL;
-
-    char  c;
-    char* cp;
-
-    if (myoptind == 0)
-        next = NULL;   /* we're starting new/over */
-
-    if (next == NULL || *next == '\0') {
-        if (myoptind == 0)
-            myoptind++;
-
-        if (myoptind >= argc || argv[myoptind][0] != '-' ||
-                                argv[myoptind][1] == '\0') {
-            myoptarg = NULL;
-            if (myoptind < argc)
-                myoptarg = argv[myoptind];
-
-            return -1;
-        }
-
-        if (strcmp(argv[myoptind], "--") == 0) {
-            myoptind++;
-            myoptarg = NULL;
-
-            if (myoptind < argc)
-                myoptarg = argv[myoptind];
-
-            return -1;
-        }
-
-        next = argv[myoptind];
-        next++;                  /* skip - */
-        myoptind++;
-    }
-
-    c  = *next++;
-    /* The C++ strchr can return a different value */
-    cp = (char*)strchr(optstring, c);
-
-    if (cp == NULL || c == ':')
-        return '?';
-
-    cp++;
-
-    if (*cp == ':') {
-        if (*next != '\0') {
-            myoptarg = next;
-            next     = NULL;
-        }
-        else if (myoptind < argc) {
-            myoptarg = argv[myoptind];
-            myoptind++;
-        }
-        else
-            return '?';
-    }
-
-    return c;
-}
-
-
-static INLINE WS_NORETURN void err_sys(const char* msg)
-{
-    printf("server error: %s\n", msg);
-
-#ifndef __GNUC__
-    /* scan-build (which pretends to be gnuc) can get confused and think the
-     * msg pointer can be null even when hardcoded and then it won't exit,
-     * making null pointer checks above the err_sys() call useless.
-     * We could just always exit() but some compilers will complain about no
-     * possible return, with gcc we know the attribute to handle that with
-     * WS_NORETURN. */
-    if (msg)
-#endif
-    {
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-static INLINE void build_addr(SOCKADDR_IN_T* addr, const char* peer,
-                              uint16_t port)
-{
-    int useLookup = 0;
-    (void)useLookup;
-
-    memset(addr, 0, sizeof(SOCKADDR_IN_T));
-
-#ifndef TEST_IPV6
-    /* peer could be in human readable form */
-    if ( (peer != INADDR_ANY) && isalpha((int)peer[0])) {
-        #ifdef CYASSL_MDK_ARM
-            int err;
-            struct hostent* entry = gethostbyname(peer, &err);
-        #else
-            struct hostent* entry = gethostbyname(peer);
-        #endif
-
-        if (entry) {
-            memcpy(&addr->sin_addr.s_addr, entry->h_addr_list[0],
-                   entry->h_length);
-            useLookup = 1;
-        }
-        else
-            err_sys("no entry for host");
-    }
-#endif
-
-#ifndef TEST_IPV6
-    #if defined(CYASSL_MDK_ARM)
-        addr->sin_family = PF_INET;
-    #else
-        addr->sin_family = AF_INET_V;
-    #endif
-    addr->sin_port = htons(port);
-    if (peer == INADDR_ANY)
-        addr->sin_addr.s_addr = INADDR_ANY;
-    else {
-        if (!useLookup)
-            addr->sin_addr.s_addr = inet_addr(peer);
-    }
-#else
-    addr->sin6_family = AF_INET_V;
-    addr->sin6_port = htons(port);
-    if (peer == INADDR_ANY)
-        addr->sin6_addr = in6addr_any;
-    else {
-        #ifdef HAVE_GETADDRINFO
-            struct addrinfo  hints;
-            struct addrinfo* answer = NULL;
-            int    ret;
-            char   strPort[80];
-
-            memset(&hints, 0, sizeof(hints));
-
-            hints.ai_family   = AF_INET_V;
-            hints.ai_socktype = udp ? SOCK_DGRAM : SOCK_STREAM;
-            hints.ai_protocol = udp ? IPPROTO_UDP : IPPROTO_TCP;
-
-            SNPRINTF(strPort, sizeof(strPort), "%d", port);
-            strPort[79] = '\0';
-
-            ret = getaddrinfo(peer, strPort, &hints, &answer);
-            if (ret < 0 || answer == NULL)
-                err_sys("getaddrinfo failed");
-
-            memcpy(addr, answer->ai_addr, answer->ai_addrlen);
-            freeaddrinfo(answer);
-        #else
-            printf("no ipv6 getaddrinfo, loopback only tests/examples\n");
-            addr->sin6_addr = in6addr_loopback;
-        #endif
-    }
-#endif
-}
-
-
-static INLINE void tcp_socket(SOCKET_T* sockFd)
-{
-    *sockFd = socket(AF_INET_V, SOCK_STREAM, 0);
-
-#ifdef USE_WINDOWS_API
-    if (*sockFd == INVALID_SOCKET)
-        err_sys("socket failed\n");
-#else
-    if (*sockFd < 0)
-        err_sys("socket failed\n");
-#endif
-
-#ifndef USE_WINDOWS_API
-#ifdef SO_NOSIGPIPE
-    {
-        int       on = 1;
-        socklen_t len = sizeof(on);
-        int       res = setsockopt(*sockFd, SOL_SOCKET, SO_NOSIGPIPE, &on, len);
-        if (res < 0)
-            err_sys("setsockopt SO_NOSIGPIPE failed\n");
-    }
-#elif defined(CYASSL_MDK_ARM)
-    /* nothing to define */
-#else  /* no S_NOSIGPIPE */
-    signal(SIGPIPE, SIG_IGN);
-#endif /* S_NOSIGPIPE */
-
-#if defined(TCP_NODELAY)
-    {
-        int       on = 1;
-        socklen_t len = sizeof(on);
-        int       res = setsockopt(*sockFd, IPPROTO_TCP, TCP_NODELAY, &on, len);
-        if (res < 0)
-            err_sys("setsockopt TCP_NODELAY failed\n");
-    }
-#endif
-#endif  /* USE_WINDOWS_API */
-}
-
-
-static INLINE void tcp_bind(SOCKET_T* sockFd, uint16_t port, int useAnyAddr)
-{
-    SOCKADDR_IN_T addr;
-
-    /* don't use INADDR_ANY by default, firewall may block, make user switch
-       on */
-    build_addr(&addr, (useAnyAddr ? INADDR_ANY : wolfsshIP), port);
-    tcp_socket(sockFd);
-
-#if !defined(USE_WINDOWS_API) && !defined(CYASSL_MDK_ARM)
-    {
-        int       res, on  = 1;
-        socklen_t len = sizeof(on);
-        res = setsockopt(*sockFd, SOL_SOCKET, SO_REUSEADDR, &on, len);
-        if (res < 0)
-            err_sys("setsockopt SO_REUSEADDR failed\n");
-    }
-#endif
-
-    if (bind(*sockFd, (const struct sockaddr*)&addr, sizeof(addr)) != 0)
-        err_sys("tcp bind failed");
-}
+#define SCRATCH_BUFFER_SZ 1200
 
 
 static uint8_t find_char(const uint8_t* str, const uint8_t* buf, uint32_t bufSz)
@@ -378,7 +84,8 @@ static int dump_stats(thread_ctx_t* ctx)
     return wolfSSH_stream_send(ctx->ssh, (uint8_t*)stats, statsSz);
 }
 
-static THREAD_RETURN CYASSL_THREAD server_worker(void* vArgs)
+
+static THREAD_RETURN WOLFSSH_THREAD server_worker(void* vArgs)
 {
     thread_ctx_t* threadCtx = (thread_ctx_t*)vArgs;
 
@@ -741,7 +448,7 @@ static void ShowUsage(void)
 }
 
 
-int main(int argc, char** argv)
+THREAD_RETURN WOLFSSH_THREAD echoserver_test(void* args)
 {
     WOLFSSH_CTX* ctx = NULL;
     PwMapList pwMapList;
@@ -751,10 +458,11 @@ int main(int argc, char** argv)
     int multipleConnections = 0;
     int useEcc = 0;
     char ch;
+    uint16_t port = wolfSshPort;
 
-    #ifdef DEBUG_WOLFSSH
-        wolfSSH_Debugging_ON();
-    #endif
+    int     argc = ((func_args*)args)->argc;
+    char**  argv = ((func_args*)args)->argv;
+    ((func_args*)args)->return_code = 0;
 
     while ((ch = mygetopt(argc, argv, "hme")) != -1) {
         switch (ch) {
@@ -793,13 +501,13 @@ int main(int argc, char** argv)
     wolfSSH_CTX_SetBanner(ctx, echoserverBanner);
 
     {
-        uint8_t buf[SCRATCH_BUFFER_SIZE];
+        uint8_t buf[SCRATCH_BUFFER_SZ];
         uint32_t bufSz;
 
         bufSz = load_file(useEcc ?
                            "./keys/server-key-ecc.der" :
                            "./keys/server-key-rsa.der",
-                          buf, SCRATCH_BUFFER_SIZE);
+                          buf, SCRATCH_BUFFER_SZ);
         if (bufSz == 0) {
             fprintf(stderr, "Couldn't load key file.\n");
             exit(EXIT_FAILURE);
@@ -821,15 +529,12 @@ int main(int argc, char** argv)
         LoadPublicKeyBuffer(buf, bufSz, &pwMapList);
     }
 
-    tcp_bind(&listenFd, SERVER_PORT_NUMBER, 0);
-
-    if (listen(listenFd, 5) != 0)
-        err_sys("tcp listen failed");
+    tcp_listen(&listenFd, &port, 0);
 
     do {
         SOCKET_T      clientFd = 0;
         SOCKADDR_IN_T clientAddr;
-        SOCKLEN_T     clientAddrSz = sizeof(clientAddr);
+        socklen_t     clientAddrSz = sizeof(clientAddr);
         THREAD_TYPE   thread;
         WOLFSSH*      ssh;
         thread_ctx_t* threadCtx;
@@ -882,5 +587,36 @@ int main(int argc, char** argv)
     return 0;
 }
 
-int myoptind = 0;
-char* myoptarg = NULL;
+#ifndef NO_MAIN_DRIVER
+
+    int main(int argc, char** argv)
+    {
+        func_args args;
+
+        args.argc = argc;
+        args.argv = argv;
+        args.return_code = 0;
+
+        StartTCP();
+
+        ChangeToWolfSshRoot();
+        #ifdef DEBUG_WOLFSSH
+            wolfSSH_Debugging_ON();
+        #endif
+
+        wolfSSH_Init();
+
+        #ifndef NO_WOLFSSH_CLIENT
+            echoserver_test(&args);
+        #endif /* NO_WOLFSSH_CLIENT */
+
+        wolfSSH_Cleanup();
+
+        return args.return_code;
+    }
+
+
+    int myoptind = 0;
+    char* myoptarg = NULL;
+
+#endif /* NO_MAIN_DRIVER */
