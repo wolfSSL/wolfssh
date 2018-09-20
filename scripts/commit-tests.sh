82fcbc130a7f6f3171f812b0b5fc3ee7318e7d75
John Safranek <john@wolfssl.com>
2018-09-20 14:19:44 -0700

    Enable All for Configure 1. Add enable options for "all" and "distro". all enables all options. distro enables all and turns on both shared and static library builds. 2. Updated the commit-tests script to perform a check on enable-all as well. 3. Made a few syntactical changes to commit-tests.

--- scripts/commit-tests.sh
+++ scripts/commit-tests.sh
@@ -1,24 +1,42 @@
 #!/bin/sh
 
-#commit-tests.sh
-
+# commit-tests.sh
 
 # make sure current config is ok
-echo -e "\n\nTesting current config...\n\n"
-make clean; make -j 8 check;
-RESULT=$?
-[ $RESULT -ne 0 ] && echo -e "\n\nCurrent config make test failed" && exit 1
+echo "Testing current config..."
+if ! make clean check
+then
+    echo "Current config make test failed"
+    exit 1
+fi
 
 
 # make sure basic config is ok
-echo -e "\n\nTesting basic config too...\n\n"
-./configure;
-RESULT=$?
-[ $RESULT -ne 0 ] && echo -e "\n\nBasic config ./configure failed" && exit 1
+echo "Testing basic config..."
+if ! ./configure
+then
+    echo "Basic config ./configure failed"
+    exit 1
+fi
 
-make -j 8 check;
-RESULT=$?
-[ $RESULT -ne 0 ] && echo -e "\n\nBasic config make test failed" && exit 1
+if ! make check
+then
+    echo "Basic config make test failed"
+    exit 1
+fi
 
+# make sure the all enabled config is ok
+echo "Testing enabled all config..."
+if ! ./configure --enable-all
+then
+    echo "Enabled all config ./configure failed"
+    exit 1
+fi
+
+if ! make check
+then
+    echo "Enabled all config make test failed"
+    exit 1
+fi
 
 exit 0
