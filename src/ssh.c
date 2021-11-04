d3c80b8fe876f31addd7d00561f7f22098346320
Jacob Barthelmeh <jacob@wolfssl.com>
2021-11-04 00:59:21 -0600

    g++ build warning fixes

--- src/ssh.c
+++ src/ssh.c
@@ -1482,7 +1482,8 @@ int wolfSSH_ReadKey_buffer(const byte* in, word32 inSz, int format,
         word32 scratch = 0;
         union wolfSSH_key *key_ptr = NULL;
 
-        key_ptr = WMALLOC(sizeof(union wolfSSH_key), heap, DYNTYPE_PRIVKEY);
+        key_ptr = (union wolfSSH_key*)WMALLOC(sizeof(union wolfSSH_key), heap,
+                DYNTYPE_PRIVKEY);
         if (key_ptr == NULL) {
             return WS_MEMORY_E;
         }
