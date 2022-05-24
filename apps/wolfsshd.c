c81fcbca39f8f154f2d0a94b391203cc8aa52093
Jacob Barthelmeh <jacob@wolfssl.com>
2022-05-24 17:05:53 -0600

    initial autotools sshd addition

---
+++ apps/wolfsshd.c
@@ -0,0 +1,47 @@
+/* wolfsshd.c
+ *
+ * Copyright (C) 2014-2021 wolfSSL Inc.
+ *
+ * This file is part of wolfSSH.
+ *
+ * wolfSSH is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wolfSSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#ifdef WOLFSSH_SSHD
+
+#include <wolfssh/ssh.h>
+#include <wolfssh/internal.h>
+#include <wolfssh/log.h>
+#include <wolfssl/wolfcrypt/wc_port.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+#ifdef NO_INLINE
+    #include <wolfssh/misc.h>
+#else
+    #define WOLFSSH_MISC_INCLUDED
+    #include "src/misc.c"
+#endif
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    printf("wolfSSH SSHD application\n");
+    return 0;
+}
+#endif /* WOLFSSH_SSHD */
