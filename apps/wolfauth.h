474527b89c09d168d873d247c8d8d2b9f9d4790d
Jacob Barthelmeh <jacob@wolfssl.com>
2022-07-06 16:31:59 -0600

    add missing wolfauth header file

---
+++ apps/wolfauth.h
@@ -0,0 +1,31 @@
+/* wolfauth.h
+ *
+ * Copyright (C) 2014-2021 wolfSSL Inc.
+ *
+ * This file is part of wolfSSH.
+ *
+ * wolfSSH is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wolfSSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#ifndef WOLFAUTH_H
+#define WOLFAUTH_H
+
+typedef struct USER_NODE USER_NODE;
+
+USER_NODE* AddNewUser(USER_NODE* list, byte type, const byte* username,
+                       word32 usernameSz, const byte* value, word32 valueSz);
+int DefaultUserAuth(byte authType, WS_UserAuthData* authData, void* ctx);
+#endif /* WOLFAUTH_H */
