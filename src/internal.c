4bbe366934cd6b4d00d90d1055ea08a42895875e
Jacob Barthelmeh <jacob@wolfssl.com>
2022-08-15 15:31:48 -0600

    fix amount copied when condensing string

--- src/internal.c
+++ src/internal.c
@@ -10478,7 +10478,7 @@ int wolfSSH_CleanPath(WOLFSSH* ssh, char* in)
     /* remove any /./ patterns, direcotries, exclude cases like ./ok./test */
     for (i = 1; i + 1 < sz; i++) {
         if (path[i] == '.' && path[i - 1] == WS_DELIM && path[i + 1] == WS_DELIM) {
-            WMEMMOVE(path + i, path + i + 1, sz - i + 1);
+            WMEMMOVE(path + i, path + i + 1, sz - (i + 1));
             sz -= 1;
             i--;
         }
