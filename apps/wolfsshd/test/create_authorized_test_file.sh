d5ecaf4452120f94ef2135327b7cbb21a6ce086f
JacobBarthelmeh <jacob@wolfssl.com>
2023-03-15 10:46:47 -0700

    update sed command with tests

--- apps/wolfsshd/test/create_authorized_test_file.sh
+++ apps/wolfsshd/test/create_authorized_test_file.sh
@@ -3,6 +3,6 @@
 USER=`whoami`
 
 cat ../../../keys/hansel-*.pub > authorized_keys_test
-sed -i "s/hansel/$USER/" ./authorized_keys_test
+sed -i.bak "s/hansel/$USER/" ./authorized_keys_test
 
 exit 0
