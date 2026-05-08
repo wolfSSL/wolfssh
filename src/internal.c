a2d4973060764a823df3a394416f674686e30d58
Yosuke Shimizu <yosuke@wolfssl.com>
2026-05-08 16:33:02 +0900

    Fix mac comparison in VerifyMac

--- src/internal.c
+++ src/internal.c
@@ -10448,7 +10448,7 @@ static INLINE int VerifyMac(WOLFSSH* ssh, const byte* in, word32 inSz,
                     ret = wc_HmacUpdate(&hmac, in, inSz);
                 if (ret == WS_SUCCESS)
                     ret = wc_HmacFinal(&hmac, checkMac);
-                if (ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
+                if (ret == WS_SUCCESS && ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
                     ret = WS_VERIFY_MAC_E;
                 break;
 
@@ -10461,7 +10461,7 @@ static INLINE int VerifyMac(WOLFSSH* ssh, const byte* in, word32 inSz,
                     ret = wc_HmacUpdate(&hmac, in, inSz);
                 if (ret == WS_SUCCESS)
                     ret = wc_HmacFinal(&hmac, checkMac);
-                if (ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
+                if (ret == WS_SUCCESS && ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
                     ret = WS_VERIFY_MAC_E;
                 break;
 
@@ -10474,7 +10474,7 @@ static INLINE int VerifyMac(WOLFSSH* ssh, const byte* in, word32 inSz,
                     ret = wc_HmacUpdate(&hmac, in, inSz);
                 if (ret == WS_SUCCESS)
                     ret = wc_HmacFinal(&hmac, checkMac);
-                if (ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
+                if (ret == WS_SUCCESS && ConstantCompare(checkMac, mac, ssh->peerMacSz) != 0)
                     ret = WS_VERIFY_MAC_E;
                 break;
 
